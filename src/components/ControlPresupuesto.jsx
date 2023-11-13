59327dca64c7fc6f355f13cbdb8a598f1d75b90d
Aidan <99897929+aidancom@users.noreply.github.com>
2023-11-13 09:09:03 +0000

    Update 6 files

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,38 +1,53 @@
-import {useState, useEffect} from 'react'
-import '../css/Control.scss'
+import { useState, useEffect } from "react";
+import "../css/Control.scss";
+import { CircularProgressbar } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
-const ControlPresupuesto = ({presupuesto, nombre, gastos}) => {
-    const [disponible, setDisponible] = useState(0)
-    const [gastado, setGastado] = useState(0)
+const ControlPresupuesto = ({ presupuesto, nombre, gastos }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+  const [porcentaje, setPorcentaje] = useState(0);
 
-    useEffect(()  => {
-        const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0);
-        const totalDisponible = presupuesto - totalGastado;
-        setGastado(totalGastado)
-        setDisponible(totalDisponible);
-        console.log(gastado)
-    }, [gastos])
-    
-    const formatearDinero = (e) => {
-        return e.toLocaleString('en-EN', {
-            currency: 'EUR',
-            style: 'currency',
-            
-        });
-    }
+  useEffect(() => {
+    const totalGastado = gastos.reduce(
+      (total, gasto) => gasto.cantidad + total,
+      0,
+    );
+    const totalDisponible = presupuesto - totalGastado;
+    setGastado(totalGastado);
+    setDisponible(totalDisponible);
+    const calculoPorcentaje = (totalGastado / presupuesto) * 100;
+    setPorcentaje(calculoPorcentaje);
+  }, [gastos, presupuesto]);
+
+  const formatearDinero = (e) => {
+    return e.toLocaleString("en-EN", {
+      currency: "EUR",
+      style: "currency",
+    });
+  };
   return (
-    <div className='control'>
-        <div className='control-grafica'>
-            <p>Grafica</p>
-        </div>
-        <div className='control-presupuesto'>
-            <p>Presupuesto de  {nombre}</p>
-            <span className='control-presupuesto-cantidad'>Tu cantidad: <span className='numero'>{formatearDinero(presupuesto)}</span></span>
-            <span className='control-presupuesto-cantidad'>Disponible: <span className='numero'>{formatearDinero(disponible)}</span></span>
-            <span className='control-presupuesto-cantidad'>Gastado: <span className='numero'>{formatearDinero(gastado)}</span></span>
-        </div>
+    <div className="control">
+      <div className="control-grafica">
+        <p>Grafica</p>
+        <CircularProgressbar value={porcentaje} text={`${porcentaje}%`} />
+      </div>
+      <div className="control-presupuesto">
+        <p>Presupuesto de {nombre}</p>
+        <span className="control-presupuesto-cantidad">
+          Tu cantidad:{" "}
+          <span className="numero">{formatearDinero(presupuesto)}</span>
+        </span>
+        <span className="control-presupuesto-cantidad">
+          Disponible:{" "}
+          <span className="numero">{formatearDinero(disponible)}</span>
+        </span>
+        <span className="control-presupuesto-cantidad">
+          Gastado: <span className="numero">{formatearDinero(gastado)}</span>
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ControlPresupuesto
+export default ControlPresupuesto;
--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,29 +1,22 @@
-import React from 'react'
-import Gasto from './Gasto'
-import '../css/ListadoGastos.scss'
+import React from "react";
+import Gasto from "./Gasto";
+import "../css/ListadoGastos.scss";
 
-const ListadoGastos = ({gastos, setEditar, eliminar}) => {
+const ListadoGastos = ({ gastos, setEditar, eliminar }) => {
   return (
-    <div className='listado'>
-        <h2>Listado de Gastos</h2>
-        {gastos.length  ? (
-         <p>Gastos</p>
-        ) : (
-        <p>No hay gastos</p>
-        )
-    
-        }
-          {gastos.map( (gasto) => (
-            <Gasto 
-              key={gasto.id}
-              gasto={e}
-              setEditar={setEditar}
-              eliminar={eliminar}
-            />
-
-           ))} 
+    <div className="listado">
+      <h2>Listado de Gastos</h2>
+      {gastos.length ? <p>Gastos</p> : <p>No hay gastos</p>}
+      {gastos.map((gasto) => (
+        <Gasto
+          key={gasto.id}
+          gasto={gasto}
+          setEditar={setEditar}
+          eliminar={eliminar}
+        />
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default ListadoGastos
+export default ListadoGastos;
